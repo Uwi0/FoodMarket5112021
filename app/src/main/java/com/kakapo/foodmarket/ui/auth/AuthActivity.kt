9b8362d58ef828cb5623add4dfd102e7e86a1328
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-05-11 15:19:47 +0700

    ganti fragment

--- app/src/main/java/com/kakapo/foodmarket/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/kakapo/foodmarket/ui/auth/AuthActivity.kt
@@ -2,8 +2,11 @@ package com.kakapo.foodmarket.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
 import com.kakapo.foodmarket.R
 import com.kakapo.foodmarket.databinding.ActivityAuthBinding
+import com.kakapo.foodmarket.ui.auth.signin.SignInFragment
 
 class AuthActivity : AppCompatActivity() {
 
@@ -13,5 +16,15 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        val pageRequest = intent.getIntExtra(SignInFragment.PAGE_REQUEST, 0)
+        if (pageRequest == SignInFragment.VALUE){
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.signInFragment, true)
+                .build()
+
+            Navigation.findNavController(findViewById(R.id.authHostFragment))
+                .navigate(R.id.action_signUp, null, navOptions)
+        }
     }
 }
--- app/src/main/java/com/kakapo/foodmarket/ui/auth/signin/SignInFragment.kt
+++ app/src/main/java/com/kakapo/foodmarket/ui/auth/signin/SignInFragment.kt
@@ -1,13 +1,20 @@
 package com.kakapo.foodmarket.ui.auth.signin
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.kakapo.foodmarket.databinding.FragmentSignInBinding
+import com.kakapo.foodmarket.ui.auth.AuthActivity
 
 class SignInFragment : Fragment() {
+
+    companion object{
+        const val PAGE_REQUEST: String = "page request"
+        const val VALUE: Int = 2
+    }
 
     private lateinit var mBinding: FragmentSignInBinding
 
@@ -18,6 +25,17 @@ class SignInFragment : Fragment() {
     ): View {
         mBinding = FragmentSignInBinding.inflate(layoutInflater, container, false)
         return mBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mBinding.btnSignup.setOnClickListener {
+            val intent = Intent(requireActivity(), AuthActivity::class.java)
+            //put extra
+            intent.putExtra(PAGE_REQUEST, VALUE)
+            startActivity(intent)
+        }
     }
 
 }
